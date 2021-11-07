e88ff0d6e9241bdf1e94efa6d85041d868050a91
xinyuan.yao <yao.ntno@gmail.com>
2021-11-07 10:51:15 +0000

    add masked_head

--- tests/solo/test_mask_head.py
+++ tests/solo/test_mask_head.py
@@ -1,5 +1,22 @@
+import torch
 from cellseg.solo.mask_head import MaskHead
 
 
 def test_mask_head() -> None:
-    ...
+    in_channels = 3
+    out_channels = 4
+    base_resolution = 4
+    num_classes = 10
+    features = [
+        torch.rand(1, in_channels, base_resolution * 2 ** i, base_resolution * 2 ** i)
+        for i in reversed(range(3))
+    ]
+    head = MaskHead(
+        in_channels=in_channels,
+        out_channels=out_channels,
+        num_classes=num_classes,
+        fpn_length=len(features),
+    )
+    res = head(features)
+    assert res.shape[2:] == features[0].shape[2:]
+    assert res.shape[:2] == (1, num_classes)
