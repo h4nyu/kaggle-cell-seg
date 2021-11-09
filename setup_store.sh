5133c1268babcefd5003c6101a0a0e6c032a4c7a
908kre <ce51046.higuchi@gmail.com>
2021-11-09 03:26:22 +0000

    fix setup_store

--- setup_store.sh
+++ setup_store.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+if [ ! -d /store ]; then
+    mkdir /store
+fi
 cd /store
-kaggle competitions download -c sartorius-cell-instance-segmentation
-unzip sartsufficient-cell-instance-segment.zip
-rm sartsufficient-cell-instance-segment.zip
+
+if [ ! -f sartorius-cell-instance-segmentation.zip ]; then
+    kaggle competitions download -c sartorius-cell-instance-segmentation
+fi
+unzip sartorius-cell-instance-segmentation.zip
+rm sartorius-cell-instance-segmentation.zip
