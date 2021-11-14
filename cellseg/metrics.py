061a6b278353a337f1f3c50111b710dac8d29b4c
908kre <ce51046.higuchi@gmail.com>
2021-11-14 11:35:33 +0000

    add metrics

--- cellseg/metrics.py
+++ cellseg/metrics.py
@@ -1,15 +1,40 @@
 import os
 import torch
 from torch import Tensor
+import numpy as np
 
 
-def seg_iou(inputs: torch.Tensor, targets: torch.Tensor) -> Tensor:
-    inputs = inputs.bool()
+def seg_iou(pred: torch.Tensor, targets: torch.Tensor) -> Tensor:
+    pred = pred.bool()
     targets = targets.bool()
-
-    intersection = (inputs & targets).sum()
-    if intersection == 0:
-        return intersection
-    union = (inputs | targets).sum()
+    intersection = (pred & targets).sum(dim=[1, 2])
+    union = (pred | targets).sum(dim=[1, 2])
     iou = intersection / union
     return iou
+
+
+def precision_at(pred_masks: Tensor, gt_masks: Tensor, threshold: float) -> float:
+    fp = torch.ones(len(gt_masks), dtype=torch.bool)
+    for mask_id, mask in enumerate(pred_masks):
+        iou = seg_iou(mask, gt_masks) > threshold
+        if iou.sum() != 0:
+            gt_mask_idx = iou.short().argmax()
+            fp[gt_mask_idx] = False
+    fp_count = fp.sum()
+    tp_count = len(gt_masks) - fp_count
+    fn_count = len(pred_masks) - tp_count
+    res = tp_count / (len(gt_masks) + len(pred_masks) - tp_count)
+    return res.item()
+
+
+def precision(
+    pred_masks: Tensor,
+    gt_masks: Tensor,
+    thresholds: list[float] = [0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95],
+) -> float:
+    precisions = [
+        precision_at(pred_masks=pred_masks, gt_masks=gt_masks, threshold=threshold)
+        for threshold in thresholds
+    ]
+    print(precisions)
+    return np.mean(precisions)
--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,6 +1,7 @@
 import pytest
 import torch
-from cellseg.metrics import seg_iou
+from torch import Tensor
+from cellseg.metrics import seg_iou, precision_at, precision
 
 
 @pytest.mark.parametrize(
@@ -21,3 +22,36 @@ def test_seg_iou(inputs_len: int, expected: float) -> None:
     targets[0, 2, 1] = 1
     res = seg_iou(inputs, targets)
     assert res == expected
+
+
+def test_seg_iou_masks() -> None:
+    inputs = torch.zeros(1, 4, 4)
+    inputs[0, 2, 0:2] = 1
+
+    targets = torch.zeros(3, 4, 4)
+    targets[0, 2, 3] = 1
+    targets[0, 2, 2] = 1
+    targets[0, 2, 1] = 1
+    res = seg_iou(inputs, targets)
+
+
+def test_precision_at() -> None:
+    inputs = torch.zeros(2, 4, 4)
+    inputs[0, 0:2, 0:2] = 1
+    inputs[1, 2, 1] = 1
+
+    targets = torch.zeros(3, 4, 4)
+    targets[0, 0, 0] = 1
+    targets[1, 1, 1] = 1
+    targets[2, 2, 2] = 1
+    res = precision_at(inputs, targets, 0.2)
+    assert res == 1 / 4
+
+
+def test_precision() -> None:
+    masks = torch.load("/app/data/masks-0030fd0e6378.pth")
+    res = precision(
+        pred_masks=masks,
+        gt_masks=masks,
+    )
+    assert res == 1.0
